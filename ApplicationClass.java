ccc63ffcbf97727004e001eaa69fb64cad529cb1
Vimal1020 <50143181+Vimal1020@users.noreply.github.com>
2020-09-14 17:06:53 +0530

    Update ApplicationClass.java

--- ApplicationClass.java
+++ ApplicationClass.java
@@ -8,8 +8,8 @@ import com.backendless.BackendlessUser;
 import java.util.List;
 
 public class ApplicationClass extends Application {
-    public static final String APPLICATION_ID = "597DFD49-973F-BC1F-FF96-076DA56B1600";
-    public static final String API_KEY = "503C3AB4-B320-455F-9C70-5841D2CB44EF";
+    public static final String APPLICATION_ID = "Your_Backendless_App_ID";
+    public static final String API_KEY = "Your_Backendless_API_KEY";
     public static final String SERVER_URL = "https://api.backendless.com";
 
     public static BackendlessUser user;
